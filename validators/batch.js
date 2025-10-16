07d78c171c363a5e3312aeaed861dbcc29c901f7
Perp-pexel <perpaman44@gmail.com>
2025-10-16 02:33:23 +0000

    batch

--- validators/batch.js
+++ validators/batch.js
@@ -1,18 +1,18 @@
 import Joi from "joi";
 
 export const createBatchValidator = Joi.object({
+  
   crop: Joi.string().required(),
   quantity: Joi.number().min(1).required(),
-  // gpsAddress: Joi.string().required(),
   collectionLocation: Joi.string().required(),
   latitude: Joi.number().optional(),
   longitude: Joi.number().optional(),
 });
 
 export const updateBatchValidator = Joi.object({
+  farmer: Joi.string().optional(),
   crop: Joi.string().optional(),
   quantity: Joi.number().min(1).optional(),
-  gpsAddress: Joi.string().optional(),
   collectionLocation: Joi.string().optional(),
   latitude: Joi.number().optional(),
   longitude: Joi.number().optional(),
